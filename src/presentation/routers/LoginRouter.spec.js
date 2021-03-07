82424770617e792959d8de97b1444e106d15ff62
Victor Sales de Brito <victor.brito@softplan.com.br>
2021-03-07 19:17:10 -0300

    Extract classes to their own files

--- src/presentation/routers/LoginRouter.spec.js
+++ src/presentation/routers/LoginRouter.spec.js
@@ -1,39 +1,5 @@
-class LoginRouter {
-  route (httpRequest) {
-    if (!httpRequest || !httpRequest.body) {
-      return HttpResponse.internalServerError()
-    }
-    const { email, password } = httpRequest.body
-    if (!email) {
-      return HttpResponse.badRequest('email')
-    }
-    if (!password) {
-      return HttpResponse.badRequest('password')
-    }
-  }
-}
-
-class HttpResponse {
-  static badRequest (paramName) {
-    return {
-      statusCode: 400,
-      body: new MissingParamError(paramName)
-    }
-  }
-
-  static internalServerError () {
-    return {
-      statusCode: 500
-    }
-  }
-}
-
-class MissingParamError extends Error {
-  constructor (paramName) {
-    super(`Missing parameter: ${paramName}`)
-    this.name = 'MissingParamError'
-  }
-}
+const MissingParamError = require('../helpers/MissingParamError.js')
+const LoginRouter = require('./LoginRouter.js')
 
 describe('Login Router', () => {
   it('should return 400 if no email is provided', () => {
